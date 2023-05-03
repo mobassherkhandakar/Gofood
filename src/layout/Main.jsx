452e08e0651cb6c891bc2c01fd1a36f40308e308
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-03 10:32:26 +0600

    meningfull footer setup done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../pages/Shared/Header/Header";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Header/>
+      <div className="min-h-[calc(100vh-60px)]">
       <Outlet/>
+      </div>
+      <Footer/>
     </div>
   );
 };
